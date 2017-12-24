0dd985be65d3009c2841a8a8d1024475e955c47b
SHYAM SUNDAR ASHOK <Shyam.sundar.Ashok@aa.com>
2017-12-24 15:53:29 -0600

    combineReducers()
    combineReducers()

--- src/components/App.js
+++ src/components/App.js
@@ -13,17 +13,14 @@ class App extends Component {
   }
 }
 
-function mapStateToProps (calendar) {
+function mapStateToProps ({food,calendar}) {
   const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
 
   return {
     calendar: dayOrder.map((day) => ({
       day,
       meals: Object.keys(calendar[day]).reduce((meals, meal) => {
-        meals[meal] = calendar[day][meal]
-          ? calendar[day][meal]
-          : null
-
+        meals[meal] = calendar[day][meal]? food[day][meal]: null
         return meals
       }, {})
     })),
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 import{
     ADD_RECIPE,
     REMOVE_FROM_CALENDAR
@@ -82,4 +84,7 @@ const initialCalendarState = {
     }
   }
 
-export default calendar
+export default combineReducers({
+  food,
+  calendar,
+})
