c8fccf8c5e866bad72716080134e07e9f4ce1a4b
SHYAM SUNDAR ASHOK <Shyam.sundar.Ashok@aa.com>
2017-12-24 15:48:26 -0600

    Adding food reducer
    Adding food reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,21 @@ import{
     REMOVE_FROM_CALENDAR
 } from '../actions';
 
+
+function food (state = {}, action) {
+  switch (action.type) {
+    case ADD_RECIPE :
+      const { recipe } = action
+
+      return {
+        ...state,
+        [recipe.label]: recipe,
+      }
+    default :
+      return state
+  }
+}
+
 const initialCalendarState = {
     sunday: {
       breakfast: null,
