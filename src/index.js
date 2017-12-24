1285c4fa247c2b91488006247cfb8cf7790f99d1
SHYAM SUNDAR ASHOK <Shyam.sundar.Ashok@aa.com>
2017-12-24 12:27:31 -0600

    Adding Redux lib,creating the store and added Redux Dev tools config
    Adding Redux lib,creating the store and added Redux Dev tools config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux';
+
+import reducer from './reducers';
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+console.log(store);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
